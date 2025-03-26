a6130f8fc0d17a0473d5174b3c4df4d21ffc227b
Kaushal Marimuthu <109944592+kaushal2m2@users.noreply.github.com>
2025-03-25 23:33:55 -0700

    rm config

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,28 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: '/_not-found',
-        destination: '/404',
-        permanent: true,
-      },
-    ];
-  },
-  webpack: (config: any, { isServer }: { isServer: boolean }) => {
-    if (!isServer) {
-      config.resolve.fallback.fs = false;
-    }
-    return config;
-  },
+  // reactStrictMode: true,
+  // // pageExtensions: ["page.tsx"],
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: '/_not-found',
+  //       destination: '/404',
+  //       permanent: true,
+  //     },
+  //   ];
+  // },
+  // webpack: (config: any, { isServer }: { isServer: boolean }) => {
+  //   if (!isServer) {
+  //     config.resolve.fallback.fs = false;
+  //   }
+  //   return config;
+  // },
 };
+// /** @type {import("next").NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   pageExtensions: [
+//       "page.tsx",
+//   ],
+// };
+
+// module.exports = nextConfig;
